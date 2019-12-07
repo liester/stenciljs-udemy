952738d1628c9fbae02c2f8295d8504df3f86b46
Levi Liester <levi.liester@gmail.com>
2019-12-06 20:02:45 -0600

    In Progress

---
+++ src/components/side-drawer/side-drawer.tsx
@@ -0,0 +1,10 @@
+import {Component, h} from "@stencil/core";
+
+@Component({
+  tag: 'uc-side-drawer'
+})
+export class SideDrawer {
+  render() {
+    return <div><h1>The side drawer</h1></div>
+  }
+}
